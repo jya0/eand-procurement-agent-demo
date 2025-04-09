f859df47af34789db4465bcc1dc31f8ee2481755
SmXzH <zhakypov1488@gmail.com>
2025-04-09 11:36:02 +0400

    refactor: improve layout and structure of supplier and eBay item cards
    - Updated the layout of the eBay item card to enhance visual clarity and organization.
    - Adjusted the supplier card to display the supplier's name more prominently.
    - Refactored the search results section to ensure consistent formatting and improved user experience.
    - Added error handling for image loading in the show_image function.

--- app_pages/Demo_.py
+++ app_pages/Demo_.py
@@ -24,8 +24,8 @@ with st.expander("Use our simple form to specify product requirements, budget, a
 # )
 
 st.header("Search for Suppliers", divider=True)
-# with st.expander("AMPA searches Alibaba's vast database to find matching suppliers instantly."):
-show_search_results()
+with st.expander("AMPA searches Alibaba's vast database to find matching suppliers instantly."):
+    show_search_results()
 
 
 # section_header(
